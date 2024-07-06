44ce4da5c6990bb87346a14d49ee2a5ee78c7f65
Avram Traian <traian.avram1@gmail.com>
2024-07-06 23:45:51 +0300

    Core: Implemented a primitive linear memory arena

---
+++ Core/Memory/Arena.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#include <Core/Memory/Arena.h>
+#include <Core/Memory/MemoryOperations.h>
+#include <Platform/Memory.h>
+
+namespace Ocean {
+
+void core_linear_arena_initialize(LinearArena& arena, usize byte_count)
+{
+    if (arena.byte_count > 0)
+        core_linear_arena_destroy(arena);
+
+    arena.byte_count = byte_count;
+    arena.memory = static_cast<ReadWriteBytes>(platform_memory_allocate(arena.byte_count));
+    arena.offset = 0;
+
+    core_linear_arena_reset(arena);
+}
+
+void core_linear_arena_destroy(LinearArena& arena)
+{
+    platform_memory_release(arena.memory, arena.byte_count);
+    arena.memory = nullptr;
+    arena.byte_count = 0;
+    arena.offset = 0;
+}
+
+void core_linear_arena_reset(LinearArena& arena)
+{
+    arena.offset = 0;
+
+    // Zero the memory that the arena will return as allocated.
+    // This ensures that the arena always returns zero-initialized memory blocks.
+    zero_memory(arena.memory, arena.byte_count);
+}
+
+void* core_linear_arena_allocate(LinearArena& arena, usize allocation_byte_count)
+{
+    constexpr usize allocation_alignment = 8;
+    const usize byte_count_after_alignment = arena.offset & (allocation_alignment - 1);
+    if (byte_count_after_alignment > 0) {
+        const usize alignment = allocation_alignment - byte_count_after_alignment;
+        if (arena.offset + alignment > arena.byte_count)
+            return nullptr;
+        arena.offset += alignment;
+    }
+
+    if (arena.offset + allocation_byte_count > arena.byte_count)
+        return nullptr;
+    void* memory_block = arena.memory + arena.offset;
+    arena.offset += allocation_byte_count;
+    return memory_block;
+}
+
+} // namespace Ocean
---
+++ Core/Memory/Arena.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <Core/Types.h>
+
+namespace Ocean {
+
+struct LinearArena {
+    ReadWriteBytes memory;
+    usize byte_count;
+    usize offset;
+};
+
+void core_linear_arena_initialize(LinearArena& arena, usize byte_count);
+void core_linear_arena_destroy(LinearArena& arena);
+void core_linear_arena_reset(LinearArena& arena);
+
+void* core_linear_arena_allocate(LinearArena& arena, usize allocation_byte_count);
+
+} // namespace Ocean
