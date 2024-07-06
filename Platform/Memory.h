da66bdb6b5e602d8ce4ff23170f07d5dcb1b4788
Avram Traian <traian.avram1@gmail.com>
2024-07-06 23:45:06 +0300

    Platform: Implemented the platform API for acquiring memory

---
+++ Platform/Memory.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <Core/Types.h>
+
+namespace Ocean {
+
+// Reserved for large allocations. Use an allocator implemented in the Core module for general purpose allocations.
+void* platform_memory_allocate(usize byte_count);
+void platform_memory_release(void* memory_block, usize byte_count);
+
+} // namespace Ocean
---
+++ Platform/Windows/WindowsMemory.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#include <Core/Assertion.h>
+#include <Platform/Memory.h>
+
+#if !OCEAN_PLATFORM_WINDOWS
+#    error Trying to include the Windows headers but they are not available!
+#endif // OCEAN_PLATFORM_WINDOWS
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+namespace Ocean {
+
+void* platform_memory_allocate(usize byte_count)
+{
+    if (byte_count == 0)
+        return nullptr;
+
+    void* memory_block = VirtualAlloc(nullptr, byte_count, MEM_COMMIT, PAGE_READWRITE);
+    VERIFY(memory_block);
+    return memory_block;
+}
+
+void platform_memory_release(void* memory_block, usize byte_count)
+{
+    if (byte_count == 0)
+        return;
+
+    VirtualFree(memory_block, 0, MEM_RELEASE);
+    (void)byte_count;
+}
+
+} // namespace Ocean
