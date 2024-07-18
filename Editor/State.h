1b51502d7994a0e2b54af9156e415d7474e683cf
Avram Traian <traian.avram1@gmail.com>
2024-07-18 19:16:03 +0300

    Editor: Implement the skeleton of the editor settings & state

---
+++ Editor/State.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <Editor/Settings.h>
+
+typedef struct EditorState {
+    EditorSettings settings;
+} EditorState;
