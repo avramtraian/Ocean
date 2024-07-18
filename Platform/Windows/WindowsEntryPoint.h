3fbbad35974924b0c0011ec4916494fcb6db4b1c
Avram Traian <traian.avram1@gmail.com>
2024-07-18 12:23:06 +0300

    Platform: Separating the Windows entry points depending on the config

--- Platform/Windows/WindowsEntryPoint.h
+++ Platform/Windows/WindowsEntryPoint.h
@@ -8,9 +8,22 @@
 #include <Core/Defines.h>
 #include <Platform/Windows/WindowsHeaders.h>
 
+#if OCEAN_CONFIGURATION_DEBUG
+
+int main()
+{
+    // NOTE: The Ocean::guraded_main() function is expected to be declared before this file is included.
+    const auto return_code = guarded_main();
+    return (INT)(return_code);
+}
+
+#else
+
 INT WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     // NOTE: The Ocean::guraded_main() function is expected to be declared before this file is included.
     const auto return_code = guarded_main();
     return (INT)(return_code);
 }
+
+#endif // OCEAN_CONFIGURATION_DEBUG
