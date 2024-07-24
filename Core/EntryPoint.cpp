9f725f5236c1bf5ea662b2c3a9187f07fa04b0c6
Avram Traian <traian.avram1@gmail.com>
2024-07-24 15:15:08 +0300

    Everywhere: Removed all drawing related code
    Starting fresh! ^^

--- Core/EntryPoint.cpp
+++ Core/EntryPoint.cpp
@@ -5,9 +5,7 @@
 
 #include <Core/Memory/Arena.h>
 #include <Core/Memory/MemoryOperations.h>
-#include <Draw/Bitmap.h>
 #include <Editor/State.h>
-#include <Platform/OffscreenBitmap.h>
 #include <Platform/Window.h>
 
 static i32 guarded_main()
@@ -16,24 +14,16 @@ static i32 guarded_main()
     core_linear_arena_initialize(&arena, 16 * MiB);
 
     Window window = platform_window_create(&arena);
-    OffscreenBitmap offscreen_bitmap = offscreen_bitmap_create(&arena, window);
 
     EditorState editor_state;
     zero_memory(&editor_state, sizeof(EditorState));
     editor_settings_initialize_default(&editor_state.settings);
 
-    while (platform_window_get_message(window)) {
-        offscreen_bitmap_resize_synced(offscreen_bitmap);
+    while (platform_window_get_message(window)) {}
 
-        bitmap_clear(offscreen_bitmap_get_bitmap(offscreen_bitmap), editor_state.settings.color_scheme.background);
-
-        offscreen_bitmap_swap(offscreen_bitmap);
-    }
-
-    offscreen_bitmap_destroy(&offscreen_bitmap);
     platform_window_destroy(&window);
-
     core_linear_arena_destroy(&arena);
+
     return 0;
 }
 
